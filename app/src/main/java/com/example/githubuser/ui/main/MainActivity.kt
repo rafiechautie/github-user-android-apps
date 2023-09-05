3039bed0512139f320e45ce866f4e46aaf03d3f1
rafiechautie <rafiqauti13@gmail.com>
2023-09-05 13:54:31 +0700

    initialize

---
+++ app/src/main/java/com/example/githubuser/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.githubuser.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubuser.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
